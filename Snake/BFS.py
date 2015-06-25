b7a07e4b468ea8a0ded9b3d7995b8b0a7db74288
TimdeJonge <t.dejonge@students.uu.nl>
2015-06-25 13:58:08 +0000

    BFS zoekt nu dichtstbijzijnde voedsel

--- Snake/BFS.py
+++ Snake/BFS.py
@@ -5,49 +5,63 @@ legend = 'urdl'
 
 level=['.##.....#.',
        '###....#..',
-       'x..x...#.#',
-       '.#..x.x#x.',
-       'x#..#...##',
-       '.........x',
-       '.......#x.',
+       '......x#.#',
+       '.#.....#..',
+       '.#..#x..##',
        '..........',
-       '........##',
+       '...x...#..',
+       '..........',
+       '......x.##',
        '#...#...#.']
 head = (0,0)
 
 
 def isWall(node):
-    if level[node[1]][node[0]] == '#':
+    if level[node[0]][node[1]] == '#':
         return True
     return False
 
+def isFood(node):
+    if level[node[0]][node[1]] == 'x':
+        return True
+    return False
+    
+
+    
 def mapLevel(start):
     frontier = PriorityQueue()
     frontier.put(start, 0)
+    foodlist = {}
     cost = {}
     came_from = {}
     cost[start] = 0
     came_from[start] = None
-    i = 0 
     while(not frontier.empty()):
         current = frontier.get()
-        #print(current, level[current[1]][current[0]])
-        
         neighbours = [((current[0] + dx[i])%10, (current[1] + dy[i])%10) for i in range(4)]
         for node in neighbours:
+            
             new_cost = cost[current] + 1
             testBool = (node not in came_from or new_cost < cost[node]) and not isWall(node) 
             if testBool:
+                if isFood(node):
+                    foodlist[node] = new_cost
                 came_from[node] = current
-                cost[node] = cost[current] + 1
-                frontier.put(node, cost[node])
+                cost[node] = new_cost
+                frontier.put(node, new_cost)
+    print(foodlist)
+    minimum = 10**6
+    for node in foodlist:
+        if foodlist[node] < minimum:
+            closest_food = node
+            minimum = foodlist[node]
+    print(closest_food)
+    return [came_from, cost, closest_food]
 
-        i+=1
-    return [came_from, cost]
 temp = mapLevel(head)
-arrowMap, lengthMap = temp[0], temp[1]
+arrowMap, lengthMap, closest_food = temp[0], temp[1], temp[2]
+
 def givePath(goal):
-    
     current = goal
     path = [current]
     while current != head:
@@ -58,7 +72,8 @@ def givePath(goal):
 
 def giveDistance(goal):
    return lengthMap[goal]
-    
-print(givePath((5,5)),giveDistance((5,5)))
+
+print(givePath(closest_food))    
+
 
 source = 'http://www.redblobgames.com/pathfinding/a-star/introduction.html'
